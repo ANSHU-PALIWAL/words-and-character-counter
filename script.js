757ccc8913d31da66b24ea74a25432a2d1cc5e66
ANSHU-PALIWAL <136322202+ANSHU-PALIWAL@users.noreply.github.com>
2023-10-10 15:10:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let textbox = document.getElementById("textbox");
+
+textbox.addEventListener('input', function () {
+    var text = this.value;
+    let char = text.length;
+    document.getElementById("character").innerHTML = char;
+
+    text = text.trim();
+    let words = text.split(" ");
+    let removegap = words.filter(function (elm) {
+        return elm != "";
+    });
+
+    document.getElementById("word").innerHTML = removegap.length;
+});
